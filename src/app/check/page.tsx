7675e83e614d07e4b757e733a04830f91d48ce76
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-23 19:54:40 +0530

    Experimentig  next js feature

---
+++ src/app/check/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Check() {
+  console.log('Check')
+  return (
+    <>
+     
+      <h1 className=" focus-visible: font-bold text-red-500 text-3xl">Check</h1>
+    </>
+  )
+}
+  
