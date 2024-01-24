ba99adbbb6ba4b8558f7d5d210acba27d418ca51
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-24 09:34:32 +0530

    Created Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Button from "@/components/ui/button";
+
 export default function Home() {
   return <>
-   <h1 className=" focus-visible: font-bold text-red-500 text-3xl">Home</h1>
+ <Button variant={'default'} size={'default'}>Button</Button>
   </>
 }
