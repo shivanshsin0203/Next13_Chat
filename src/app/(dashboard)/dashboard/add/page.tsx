3a5a5690730b517aa41d480c04bfdf5e7931719b
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-25 18:07:26 +0530

    Added forms and handle freind button

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,11 @@
+import AddFriendButton from "@/components/ui/AddFriendButton";
+import { FC } from "react";
+
+const Page: FC = () => {
+  return <>
+  <main>
+    <h1>Add Friend</h1>
+    <AddFriendButton />
+  </main>
+  </>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import Providers from '@/components/Providers'
 import './globals.css'
 
-// Done after the video and optional: add page metadata
+
 export const metadata = {
   title: 'FriendZone | Home',
   description: 'Welcome to the FriendZone',
