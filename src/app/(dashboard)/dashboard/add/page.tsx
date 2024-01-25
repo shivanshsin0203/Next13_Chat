3d42dc0a0372a3442cb489ab55bac0de240eeec7
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-25 18:51:33 +0530

    Created friend request routes

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,11 +1,13 @@
-import AddFriendButton from "@/components/AddFriendButton";
-import { FC } from "react";
+import AddFriendButton from '@/components/AddFriendButton'
+import { FC } from 'react'
 
-const Page: FC = () => {
-  return <>
-  <main>
-    <h1>Add Friend</h1>
-    <AddFriendButton />
-  </main>
-  </>
+const page: FC = () => {
+  return (
+    <main className='pt-8'>
+      <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+      <AddFriendButton />
+    </main>
+  )
 }
+
+export default page
